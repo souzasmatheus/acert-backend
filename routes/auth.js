3da3b1880dedd4196302e0d801b56377d0f3e1d8
Matheus Souza <matheu.sk.9@hotmail.com>
2020-01-22 19:31:00 -0300

    Create login route

--- routes/auth.js
+++ routes/auth.js
@@ -46,4 +46,36 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    try {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (isMatch) {
+        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+        res
+          .header('auth-token', token)
+          .status(200)
+          .send({ message: 'You are now logged in', user });
+      } else {
+        res.status(400).send({
+          message: 'E-mail or password incorrect'
+        });
+      }
+    } catch (err) {
+      res.status(400).send({
+        message: 'There has been an error with the authentication'
+      });
+    }
+  } else {
+    res.status(400).send({
+      message: 'E-mail or password incorrect'
+    });
+  }
+});
+
 module.exports = router;
